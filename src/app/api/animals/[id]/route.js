2ffd678df50e008f1a4d0f54e1e4d7f04841dc6c
danisnake <adriandanielfernandez@gmail.com>
2023-10-05 22:11:38 -0300

    vamooo

--- src/app/api/animals/[id]/route.js
+++ src/app/api/animals/[id]/route.js
@@ -1,12 +1,35 @@
-import { NextResponse } from "next/server"
-import {prisma} from "@/libs/prisma"
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+import { GET } from "../route";
 
-export function GET() {
-    return NextResponse.json("opteniendo por id animales")
+export async function GET(request, { params }) {
+  const getAnimalId = await prisma.animal.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+  return NextResponse.json(getAnimalId);
 }
-export function PUT() {
-    return NextResponse.json("actualizando animales")
+
+export async function PUT(request, { params }) {
+  const data = await request.json();
+  const updateAnimal = await prisma.animal.update({
+    where: {
+      id: Number(params.id),
+    },
+    data: data,
+  });
+  return NextResponse.json(updateAnimal);
 }
-export function DELETE() {
-    return NextResponse.json("borrando animales")
+export async function DELETE() {
+  try {
+    const deleteAnimal = await prisma.animal.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    return NextResponse.json(deleteAnimal);
+  } catch (error) {
+    return NextResponse.json(error.messenge);
+  }
 }
